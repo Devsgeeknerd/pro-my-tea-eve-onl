e49feb9109c0cb763f8e37095bacbede09e6d0e2
Paulo Vanderley <vanderley.1109@gmail.com>
2022-10-10 12:21:37 -0300

    adicionando next-env

---
+++ frontend/next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited.
+// See https://nextjs.org/doc/basic-features/typescript for more information.
