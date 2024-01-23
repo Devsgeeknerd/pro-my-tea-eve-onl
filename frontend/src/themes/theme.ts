b55395d7180a0e8ea866682ae831d32b4fb1a4ff
Paulo <vanderley.1109@gmail.com>
2024-01-23 15:51:18 -0300

    atualizando conteudo

--- frontend/src/themes/theme.ts
+++ frontend/src/themes/theme.ts
@@ -1,20 +1,28 @@
+// Importa a função createTheme do pacote @mui/material.
 import { createTheme } from "@mui/material";
 
+// Cria um tema personalizado utilizando a função createTheme.
 const theme = createTheme({
+  // Define a paleta de cores do tema.
   palette: {
+    // Define a cor principal (primária) com o código hexadecimal #f52273.
     primary: {
       main: "#f52273",
     },
+    // Define a cor secundária com o código hexadecimal #f5f5f5.
     secondary: {
       main: "#f5f5f5",
     },
+    // Define a cor do texto principal com o código hexadecimal #f52273.
     text: {
       primary: "#f52273",
     },
   },
+  // Define objetos vazios para as propriedades shape, components e MuiTextField.
   shape: {},
   components: {},
   MuiTextField: {},
 });
 
+// Exporta o tema criado para que possa ser utilizado em outros arquivos.
 export default theme;
